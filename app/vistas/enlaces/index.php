ab2cb20c9848076bad0c382737759a398ce2b3cc
Jorge Rodríguez Sanz <jergo23@gmail.com>
2016-03-06 16:19:31 +0100

    Controlador de enlaces

---
+++ app/vistas/enlaces/index.php
@@ -0,0 +1,34 @@
+<div id="enlaces">
+    <h2 class="titulo_seccion"><?php echo iText('Enlaces', 'dicc'); ?></h2>
+    <?php
+        echo \core\HTML_Tag::a_boton("boton", array("enlaces", "form_anexar"), "Incluir un nuevo enlace");
+    ?>
+    <dl> 
+        <?php
+            foreach ( $datos['enlaces'] as $id => $enlace ) {
+                $patron = '/http:\/\//';
+                if(! preg_match($patron, $enlace['url'])){
+                    $enlace['url'] = 'http://'.$enlace['url'];
+                }
+                echo "<dt>
+                        <a href='{$enlace['url']}' target='on_blank' title='{$enlace['descripcion']}'>
+                            {$enlace['titulo']}
+                        </a>
+                    </dt>
+                    <dd>{$enlace['descripcion']}</dd>
+                    <center>
+                        ".\core\HTML_Tag::a_boton("", array("enlaces", "form_modificar", $id), "Modificar")
+                            ." - "
+                         .\core\HTML_Tag::a_boton("", array("enlaces", "form_borrar", $id), "Borrar")."
+                    </center>
+                    ";
+            }
+        ?>
+    </dl>
+    <center>
+        <?php
+            echo \core\HTML_Tag::a_boton("boton1", array("enlaces", "form_anexar"), "Incluir un nuevo enlace");
+        ?>
+    </center>  
+    <!--<center><button onclick='<?php //echo URL_ROOT.\core\Distribuidor::get_controlador_instanciado()."/form_anexar" ?>'>Incluir un nuevo enlace</button></center>-->
+</div>
--- app/vistas/zonas/menu_izq.php
+++ app/vistas/zonas/menu_izq.php
@@ -1,7 +1,8 @@
 <!--<h3><?php //echo iText('leyenda_menu', 'dicc'); ?></h3>-->
 <ul id="menu" class="menu">
     <?php echo \core\HTML_Tag::li_menu("item", array("inicio"), "Inicio"); ?>
-    <li><span title="<?php echo iText('Equipos', 'dicc'); ?>"><?php echo iText('Equipos', 'dicc'); ?></span>       <ul id="list_team">
+    <li><span title="<?php echo iText('Equipos', 'dicc'); ?>"><?php echo iText('Equipos', 'dicc'); ?></span>
+        <ul id="list_team">
             <?php echo \core\HTML_Tag::li_menu("subitem", array("teams"), "Todos"); ?>
             <?php
             $teams = new \modelos\teams();
@@ -20,6 +21,7 @@
     <?php echo \core\HTML_Tag::li_menu("item", array("players"), "Jugadores"); ?>
     <?php echo \core\HTML_Tag::li_menu("item", array("skills"), "Habilidades"); ?>
     <?php echo \core\HTML_Tag::li_menu("item", array("tacticas"), "Tácticas"); ?>
+    <?php echo \core\HTML_Tag::li_menu("item", array("enlaces"), "Links"); ?>
     <?php echo \core\HTML_Tag::li_menu("menu_adm", array("usuarios"), "Usuarios"); ?>
 </ul>
 
