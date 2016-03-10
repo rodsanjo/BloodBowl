f2592e2c3410e040e6861cdc1ee7bc13cb9aa50a
Jorge Rodríguez Sanz <jergo23@gmail.com>
2016-03-10 20:01:02 +0100

    Tabla general de habilidades

--- app/vistas/skills/index.php
+++ app/vistas/skills/index.php
@@ -10,7 +10,58 @@ if ( \core\Usuario::tiene_permiso('skills', 'altasCSV')) {
 }
 echo \core\HTML_Tag::a_boton_onclick("btn_derch button", array($controlador, "form_insertar"), "<span class='glyphicon glyphicon-send' aria-hidden='true'> +</span>", array('title' => 'Nueva habilidad'));
 
+//habilidades por tipo:
+$tipos['G'][] = 'Generales';
+$tipos['P'][] = 'Pase';
+$tipos['A'][] = 'Agilidad';
+$tipos['F'][] = 'Fuerza';
+$tipos['M'][] = 'Mutación';
+$tipos['O'][] = 'Otros';
+foreach ($datos['habilidades'] as $key => $hab) {
+    switch ($hab['tipo']) {
+        case 'G':
+            $tipos['G'][] = $hab['nombre'];
+            break;
+        case 'P':
+            $tipos['P'][] = $hab['nombre'];
+            break;
+        case 'A':
+            $tipos['A'][] = $hab['nombre'];
+            break;
+        case 'F':
+            $tipos['F'][] = $hab['nombre'];
+            break;
+        case 'M':
+            $tipos['M'][] = $hab['nombre'];
+            break;
+        default:
+            $tipos['O'][] = $hab['nombre'];
+            break;
+    }
+}
+
+
 ?>
+    <div id="lista_habs">
+        <h2>Categorías de habilidades</h2>
+        <?php
+        foreach ($tipos as $tipo => $habilidades) {
+            if( count($habilidades) > 1 ){
+                echo "<ul>";
+                echo "<li><h3 style='clear: both;'>{$habilidades[0]}</h3>";
+                unset($habilidades[0]);
+                foreach ($habilidades as $key => $hab_nomb) {
+                    //$hab_nombr = str_replace(" ", "", $hab_nomb);
+                    echo "<div class='col-lg-2 col-md-3 col-sm-4'><a href='#$hab_nomb'>$hab_nomb</a></div>";
+                }
+                echo "</ul><br/>";
+            }
+        }
+        echo "<br/>";
+        ?>
+    </div>
+
+
 <div class="col-md-12">
     <table class="table table-striped">
     <thead>
@@ -30,7 +81,8 @@ echo \core\HTML_Tag::a_boton_onclick("btn_derch button", array($controlador, "fo
         foreach ($datos['habilidades'] as $key => $hab) {
             echo '<tr>';
                 //echo "<td class='edicion_jugador' data-id='{$jugador['id']}'>{$jugador['nombre']} <span class='glyphicon glyphicon-pencil' aria-hidden='true' style='color:#428bca;'></span></td>";
-                echo "<td>{$hab['nombre']}</td>";
+                //$hab_nombr = str_replace(" ", "", $hab['nombre']);
+                echo "<td id='{$hab['nombre']}'>{$hab['nombre']}</td>";
                 echo "<td>{$hab['tipo']}</td>";
                 echo "<td>{$hab['descripcion']}</td>";
                 if ( \core\Usuario::tiene_permiso('skills', 'form_borrar')) {
